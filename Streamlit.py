0c35bb047790f67c9e73636404c42aad0e2bace4
Korsholm22 <nicklas@anb.dk>
2022-10-04 15:31:34 +0200

    new commit...

--- Streamlit.py
+++ Streamlit.py
@@ -1,12 +1,13 @@
+import pandas as pd
+import pickle 
+from xgboost import XGBRegressor 
+from sklearn.preprocessing import StandardScaler
+import shap 
+
+import numpy as np
+import itertools 
 import streamlit as st 
 
 st.title("Hello")
 st.write("Hello App!")
 
-tabs1, tabs2 = (["Tab1, tab2"])
-
-with tabs1:
-    st.header("Hello")
-
-with tabs2:
-    st.header("Hello2")
