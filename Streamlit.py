48a3971fb10f1d24107eedac518cbb5f2565df33
Korsholm22 <nicklas@anb.dk>
2022-10-04 12:11:23 +0200

    plz fix

--- Streamlit.py
+++ Streamlit.py
@@ -10,5 +10,7 @@ from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
 import shap
 import pickle
-import streamlit as st 
+import streamlit as st
+
+st.title("Hello")
 st.write("Hello App!")
