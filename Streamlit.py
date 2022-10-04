9c9e8cea86bd6baedf70f65618ccb0c855879395
Korsholm22 <nicklas@anb.dk>
2022-10-04 15:29:12 +0200

    new commit

--- Streamlit.py
+++ Streamlit.py
@@ -1,11 +1,3 @@
-import pandas as pd
-import pickle 
-from xgboost import XGBRegressor 
-from sklearn.preprocessing import StandardScaler
-import shap 
-
-import numpy as np
-import itertools 
 import streamlit as st 
 
 st.title("Hello")
